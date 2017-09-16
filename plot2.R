dc10b37032baba2b0679437ff7b771e46900eeea
michannecurrie <31278296+michannecurrie@users.noreply.github.com>
2017-09-16 15:35:49 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -14,3 +14,6 @@ with(data1, {
   plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
 })
+
+dev.copy(png,file="plot2.png",width=480,height=480)
+dev.off()
