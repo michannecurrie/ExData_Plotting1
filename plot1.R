a812f997463f515d91b4cf2d22ee3ad246afa1d8
michannecurrie <31278296+michannecurrie@users.noreply.github.com>
2017-09-16 15:10:37 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@ data_full<-read.csv("household_power_consumption.txt",header=T,sep=';',
                     na.strings="?",nrows=2075259,
                     check.names=F,stringsAsFactors=F,comment.char="",quote='\"')
 ##Subsetting the data
-data1<-subset(data_full,Date %in% c("1/2/2007","2/2/2001"))
+data1<-subset(data_full,Date %in% c("1/2/2007","2/2/2007"))
 
 data1$Date<-as.Date(data1$Date, format="%d %m %y")
 ##Plotting of data
